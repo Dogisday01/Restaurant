004a2a9b67581dac2eb4f491f38bd09a5ee7d640
Damir <ospanbekdamir1@gmail.com>
2023-02-25 13:34:03 +0600

    class main - launches applications

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
-
-
-import java.sql.SQLException;
-import java.util.Scanner;
+import Application.Application;
+import Controllers.MenuController;
+import Controllers.UserController;
+import Data.Interface.IDB;
+import Data.Mysql;
+import Repositories.UserRepository.Interface.IUserRepository;
+import Repositories.UserRepository.*;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
+
+
+        IDB db = new Mysql();
+
+
+        IUserRepository userRepo = new UserRepository(db);
+        UserController userController = new UserController(userRepo);
+        MenuController menuController = new MenuController();
+
+
+        Application app = new Application(userController, menuController);
+        app.start();
 
     }
 }
