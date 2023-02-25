3f127c8464a093717dd5ad9d22c9a90b7f4003cf
Damir <ospanbekdamir1@gmail.com>
2023-02-25 10:38:58 +0600

    Package Data: class Mysql - to connect to the MySql database

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-import MarketPlace.Application;
-import Users.UserRepository;
-import Users.Interface.IUserRepository;
+
 
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        InsertMenuItems menu = new InsertMenuItems();
-        Application app = new Application();
-        app.run();
+
     }
 }
